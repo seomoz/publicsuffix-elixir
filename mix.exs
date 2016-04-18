952dfbec9a0c304975093b1067dbfd8916eac244
Myron Marston <myron.marston@gmail.com>
2016-04-18 12:49:43 -0700

    First pass at implementing logic.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,12 @@ defmodule PublicSuffix.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [
+        :idna,
+        :logger,
+      ]
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -23,10 +28,41 @@ defmodule PublicSuffix.Mixfile do
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "1.1.0"}
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:idna, "~> 1.2"},
+    ]
+  end
+end
+
+defmodule Mix.Tasks.PublicSuffix.SyncFiles do
+  use Mix.Task
+
+  @shortdoc "Syncs the files from publicsuffix.org"
+  @data_dir Path.expand("data", __DIR__)
+
+  def run(_) do
+    File.mkdir_p!(@data_dir)
+    sync_file "https://publicsuffix.org/list/public_suffix_list.dat", "public_suffix_list.dat"
+    sync_file "https://raw.githubusercontent.com/publicsuffix/list/master/tests/test_psl.txt", "tests.txt"
+  end
+
+  defp sync_file(remote_url, local_path) do
+    local_path = Path.join(@data_dir, local_path)
+
+    [
+      "curl",
+      "-s",
+      remote_url,
+      "--output",
+      local_path,
+    ]
+    |> Enum.join(" ")
+    |> Mix.shell.cmd
+
+    IO.puts "Synced #{remote_url} to #{local_path}"
   end
 end
--- test/public_suffix_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PublicSuffixTest do
-  use ExUnit.Case
-  doctest PublicSuffix
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
