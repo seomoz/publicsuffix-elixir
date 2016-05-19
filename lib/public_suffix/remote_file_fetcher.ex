0e9d36013d021104d44fd2ad6a85b376ff65747d
Myron Marston <myron.marston@gmail.com>
2016-05-18 22:09:20 -0700

    Doc improvements.
    - Indent examples so they render as code.
    - Hide `PublicSuffix.RemoteFileFetcher` in docs.
    - Ignore doc directory.

--- lib/public_suffix/remote_file_fetcher.ex
+++ lib/public_suffix/remote_file_fetcher.ex
@@ -1,4 +1,6 @@
 defmodule PublicSuffix.RemoteFileFetcher do
+  @moduledoc false
+
   def fetch_remote_file(url) when is_binary(url) do
     # These are not listed in `applications` in `mix.exs` because
     # this is only used at compile time or in one-off mix tasks --
