5340724c05e84c22c5c9d0159bc4b0d611dc381a
Myron Marston <myron.marston@gmail.com>
2016-05-18 21:46:51 -0700

    Configure hex package metadata.

--- mix.exs
+++ mix.exs
@@ -3,16 +3,15 @@ defmodule PublicSuffix.Mixfile do
 
   def project do
     [app: :public_suffix,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [
       # :idna is intentionally NOT included in this list because it is
@@ -24,21 +23,27 @@ defmodule PublicSuffix.Mixfile do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "1.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:idna, "~> 2.0"},
       # ex_doc and earmark are necessary to publish docs to hexdocs.pm.
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
+    ]
+  end
+
+  defp description do
+    """
+    Operate on domain names using the public suffix rules provided by https://publicsuffix.org/.
+    """
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Myron Marston", "Ben Kirzhner"],
+      links: %{"GitHub" => "https://github.com/seomoz/publicsuffix-elixir",
+               "Public Suffix List" => "https://publicsuffix.org/"},
     ]
   end
 end
